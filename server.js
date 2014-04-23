cca4235c305032e3ae0e700814adfbabba0c8394
Doctor Cobweb <spinninghalf@gmail.com>
2014-04-23 15:12:11 +1000

    implementing oauth2 access

--- server.js
+++ server.js
@@ -5,8 +5,20 @@
 var applicationRoot = __dirname,
     path = require('path'),
     express = require('express'),
+    request = require('request'),
     PORT = process.env.PORT || 5000;
 
+
+var baseUri = 'https://agtest.nationbuilder.com/';
+var responseType = 'code';
+var grantType = 'authorization_code';
+
+var authorizeUri = baseUri + 'oauth/authorize' +
+                  '?response_type=' + responseType +
+                  '&client_id=' + process.env.CLIENT_ID +
+                  '&redirect_uri=' + process.env.REDIRECT_URI;
+
+var accessTokenUri = baseUri + 'oauth/token';
 
 var app = express();
 
@@ -22,10 +34,39 @@ app.configure(function () {
 
 app.get('/oauth2callback', function (req, res) {
     console.log('in /oauth2callback route handler');
-    console.log(req.query);
+    console.log(req.query.code);
 
-    //return res.send('hello');
-    return res.redirect('leadorganizerapp://oauth2authorization?code=' + req.query.code);
+    var postBody = {
+        'client_id': process.env.CLIENT_ID,
+        'redirect_uri': process.env.REDIRECT_URI,
+        'grant_type': grantType,
+        'client_secret': process.env.CLIENT_SECRET,
+        'code': req.query.code
+    };
+
+    console.log('postBody:');
+    console.log(postBody);
+
+    var options = {
+        url: accessTokenUri,
+        method: 'POST',
+        json: postBody
+    };
+
+    function callback(error, response, body) {
+        console.log('in callback of POST request to get access token');
+        if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body);
+            console.log('info:');
+            console.log(info);
+    
+            return res.redirect('https://www.google.com.au');
+        }
+    }
+    
+    console.log('making request to nation builder to get access token.');
+    request(options, callback);
+    //return res.redirect('leadorganizerapp://oauth2authorization?code=' + req.query.code);
 });
 
 
